eb985b6539473d8e7927cae1514e4e75f539307a
Charles026 <charles0260@outlook.com>
2023-06-03 02:11:19 +0800

    add baselayout, css,tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    integrations: [
+        react(),
+        image({ serviceEntryPoint: '@astrojs/image/sharp'}),
+        tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		})],
+});
